2886f1f3c32cd32923bab9742e5d12ca6930fe61
Flávia Gomes <flagomesbh@gmail.com>
2021-11-20 16:27:32 -0300

    Add detalhes

--- BibliotecaMVC/Service/BibliotecaService.cs
+++ BibliotecaMVC/Service/BibliotecaService.cs
@@ -6,12 +6,12 @@ namespace BibliotecaMVC.Service
 {
     public class BibliotecaService
     {
-        private const string url = "https://itunes.apple.com/search";
+        private const string url = "https://itunes.apple.com/";
 
         public Response BuscaLivro(string textoPesquisa)
         {
             Response retorno = null;
-            string parametros = $"?term={textoPesquisa}&entity=ibook";
+            string parametros = $"search?term={textoPesquisa}&entity=ibook";
             var newUrl = $"{url}{parametros}";
 
             HttpClient clienteApi = new()
@@ -25,6 +25,27 @@ namespace BibliotecaMVC.Service
                 retorno = resultado.Content.ReadFromJsonAsync<Response>().Result;
             }
             
+            clienteApi.Dispose();
+            return retorno;
+        }
+
+        public Response BuscaLivroPeloId(int id)
+        {
+            Response retorno = null;
+            string parametros = $"lookup?id={id}";
+            var newUrl = $"{url}{parametros}";
+
+            HttpClient clienteApi = new()
+            {
+                BaseAddress = new Uri(url)
+            };
+
+            var resultado = clienteApi.GetAsync(newUrl).Result;
+            if (resultado.IsSuccessStatusCode)
+            {
+                retorno = resultado.Content.ReadFromJsonAsync<Response>().Result;
+            }
+
             clienteApi.Dispose();
             return retorno;
         }
